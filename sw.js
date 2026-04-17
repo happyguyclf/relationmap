432198a5396e1c9703230eb08b29d9607b6f6420
happyguyclf <clfpierre19@gmail.com>
2026-04-18 00:07:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,18 @@ const CACHE_NAME = 'mindmap-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/icon-512.png'
+  '/iconApp.png'
 ];
-
 // Install Event: Save files to phone storage
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
-
 // Fetch Event: Serve files from storage if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
+
